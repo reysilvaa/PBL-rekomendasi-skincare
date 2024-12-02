bc5eb77bc179935d2befb7f6320f195ca2269e5d
reysilvaa <reynaldsilva123@gmail.com>
2024-12-02 10:24:33 +0700

    config changed

--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,8 +1,11 @@
-// class Config {
-//   static const String baseUrl = 'http://127.0.0.1:8001/api'; // Base URL API
-// }
+import 'dart:io';
 
 class Config {
-  static const String baseUrl =
-      'https://rnquf-2001-448a-50b0-1b72-d4e5-fee4-7d40-b709.a.free.pinggy.link/api'; // Base URL API
+  static const String rawUrl = 'http://127.0.0.1:8001/api';
+  static String baseUrl = Platform.isAndroid ? rawUrl : rawUrl; // Base URL API
 }
+
+// class Config {
+//   static const String baseUrl =
+//       'https://rnsfy-114-6-25-184.a.free.pinggy.link/api'; // Base URL API
+// }
